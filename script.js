8743baac6c3835e2fb23403a3db2333f5c3a2a30
kvk98 <119598315+kvk98@users.noreply.github.com>
2023-10-11 14:18:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -32,10 +32,10 @@ const lookup = {
 };
 
 function rot13(encodedStr) {
-  let decodedArr = []; // Your Result goes here
+  let decodedArr = ""; // Your Result goes here
   // Only change code below this line
 	for (let i = 0; i < encodedStr.length; i++) {
-		decodedArr[i] += encodedStr[i+13];
+		decodedArr += lookup[encodedStr[i]];
 	}
 
   return decodedArr; //return decodedArr
